43326b51f81b01a54fcd3e0f1e024fcde7b3da5e
João Pedro Alcântara <alcantara.joaop30@gmail.com>
2025-02-02 22:49:08 -0300

    fixup! refactor: Allow user to define azs to use

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,4 +1,4 @@
 locals {
-  filtered_subnets = [for subnet in aws_subnet.private_subnets : subnet.id if subnet.availability_zone != "${data.aws_region.current.name}e"]
+  filtered_subnets = [for subnet in aws_subnet.private_subnets : subnet.id if subnet.availability_zone != "us-east-1e"]
   len_azs          = length(var.azs)
 }
